6698147e6fdb7d6246b2e788d68d141929f6bf2e
methodmenon <methodmenon@gmail.com>
2014-08-18 21:08:38 -0700

    removed the get_recent_retweets function fully. Added the function get_tweet, which returns a single tweet speicified by an id parameter. Added another function retweet, which retweets a particular tweet.

--- tweetful.py
+++ tweetful.py
@@ -5,22 +5,29 @@ import sys
 import authorization
 from urls import *
 
-def get_retweets(tweet_id):
-    auth = authorization.authorize()
-    """retrieve up to the last 100 retweets of a particular tweet specified by the id parameter"""
-    recent_retweet_url = RECENT_RETWEET_URL
-    recent_retweet_url = recent_retweet_url.format(tweet_id = tweet_id)
-    return requests.get(recent_retweet_url, auth=auth)
 
-#def retweet(tweet_id):
+def get_tweet(tweet_id):
+    "returns a single tweet, specified by the id parameter"
+    auth = authorization.authorize()
+    get_tweet_url = GET_TWEET_URL
+    get_tweet_url = get_tweet_url.format(tweet_id = tweet_id)
+    return requests.get(get_tweet_url, auth=auth)
+
+def retweet(tweet_id):
+    "retweets a tweet"
+    auth = authorization.authorize()
+    retweet_url = RETWEET_URL
+    retweet_url = retweet_url.format(tweet_id = tweet_id)
+    return requests.get(retweet_url, auth=auth)
+
+
 
  
 def main():
     """ Main function """
-    #retweet id for the moment: 500081119417470976
+    #tweet id for the moment: 500081119417470976
     auth = authorization.authorize()
-    response = get_retweets(sys.argv[1])
-    #response = requests.get(TIMELINE_URL, auth=auth)
+    response = get_tweet(sys.argv[1])
     print json.dumps(response.json(), indent=4)
  
 if __name__ == "__main__":
