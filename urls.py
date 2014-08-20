13d59c3b642c15a9cb706b38cc2e304122756426
methodmenon <methodmenon@gmail.com>
2014-08-19 21:01:31 -0700

    Removed unnecessary variable GET_RETWEETS_URL since it is not useful anymore.

--- urls.py
+++ urls.py
@@ -5,6 +5,5 @@ AUTHORIZE_URL = API_URL + "/oauth/authorize?oauth_token={request_token}"
 ACCESS_TOKEN_URL = API_URL + "/oauth/access_token"
 TIMELINE_URL = API_URL + "/1.1/statuses/home_timeline.json"
 #the rest are for the twitter extension
-RECENT_RETWEET_URL = API_URL + "/1.1/statuses/retweets/{tweet_id}.json"
 GET_TWEET_URL = API_URL + "/1.1/statuses/show/{tweet_id}.json"
 RETWEET_URL = API_URL + "/1.1/statuses/retweet/{tweet_id}.json"
